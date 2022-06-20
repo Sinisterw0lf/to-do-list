e0d926d47d019006c1659e0586833477d4a90aa6
Sinisterw0lf <devilhell76@gmail.com>
2022-06-20 10:58:45 +0200

    Finished

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-// document.getElementById("push").onclick = function (e) {
-//   console.log("e",e);
-// };
-
-document.getElementById("push").addEventListener("click", function () {
+// add task
+document.getElementById("push").onclick = function () {
   let elInput = document.getElementById("to-do-input").value;
-  console.log(elInput.length);
+  // console.log(elInput.length);
   if (elInput == "") {
     alert("Ajoute une tache");
   } else if (elInput.length < 3) {
@@ -14,19 +11,29 @@ document.getElementById("push").addEventListener("click", function () {
   } else {
     console.log("Tout est ok pour envoyer !");
     document.querySelector("#tasks").innerHTML += `
-    <div class="d-flex justify-content-between align-items-center mb-5">
-    <span id="taskName" class="my-0">${elInput}</span>
-    <button class="btn btn-danger">Delete</button>   
+    <div class="d-flex justify-content-between align-items-center py-3 mb-3 border-bottom">
+    <span  class="my-0 taskname">${elInput}</span>
+    <button class="btn btn-danger delete">Delete</button>   
     </div>
     `;
-  }
-});
+    // delete task
+    let tasksDelete = document.querySelectorAll(".delete");
+    console.log("tasksDelete : ", tasksDelete);
+    for (let i = 0; i < tasksDelete.length; i++) {
+      tasksDelete[i].onclick = function () {
+        console.log("click");
+        this.parentNode.remove();
+      };
+    }
+    // task done
+    let tasksDone = document.querySelectorAll(".taskname");
+    for (let i = 0; i < tasksDone.length; i++) {
+      tasksDone[i].onclick = function () {
+        console.log("Task Done");
+        this.classList.toggle("completed");
+      };
+    }
 
-// let currentTasks = document.querySelectorAll(".delete");
-// for (let index = 0; index < tasks.length; index++) {
-//     tasks[index].onclick= function  {
-        
-//     }
-//     const element = tasks[index];
-    
-// }
+    document.querySelector("#to-do-input").value = "";
+  }
+};
